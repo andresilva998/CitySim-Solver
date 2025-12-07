7e364407fb49cc0aa8c80410bc572d1e768d1b5a
andresilva998 <andre.filipe.rodrigues.silva@gmail.com>
2025-12-07 11:56:36 +0000

    BIPV/T update

--- models.cpp
+++ models.cpp
@@ -527,10 +527,10 @@ void Model::ThermalStepImplicitTemperature(Ground *pGround, Climate* pClimate, u
     }
 
     // evapotranspiration (ET) modification term by GU
-    Aprime[0] = pGround->getCapacitance(0,0) - dt*( -pGround->getk(1) - pGround->getG(0)*(pGround->get_hr()+pGround->get_hc())/((1.+Lambda)*pGround->getG(0)+pGround->get_hr()+pGround->get_hc()) );
+    Aprime[0] = pGround->getCapacitance(0,0) - dt*( -pGround->getk(1) - pGround->getG(0)*(pGround->get_hr()+pGround->get_hc())/((1.+Lambda)*pGround->getG(0)+pGround->get_hr()+pGround->get_hc()) );␊
 
-    solve_Ax_equal_b(Aprime.data(), bprime.data(), NP);
-
+    solve_Ax_equal_b(Aprime, bprime, NP);
+	
     // saves the results in the ground
     for (int i=0; i<NP; ++i) {
         pGround->setLayerTemperature(i,bprime[i]);
@@ -3826,6 +3826,7 @@ void Model::computeCMIndices(Building* pBuilding, Climate* pClimate, unsigned in
 
     return;
 }
+
 
 
 
